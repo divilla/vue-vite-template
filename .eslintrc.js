92cbf62f0772be9ce65623a9b7df844373081d42
Vito Di Villa <vitodivilla@gmail.com>
2024-10-04 09:06:48 +0200

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  globals: {
+    defineEmits: true,
+    document: true,
+    localStorage: true,
+    GLOBAL_VAR: true,
+    window: true,
+    defineProps: true,
+    defineExpose: true,
+  },
+  extends: ['./.eslintrc-auto-import.json', 'airbnb-base', 'plugin:vue/vue3-recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['vue', 'import'],
+  rules: {
+    'no-undef': 0,
+    'vue/no-multiple-template-root': 0,
+    'no-console': 0,
+    'import/no-unresolved': 0,
+    'import/extensions': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/prefer-default-export': 0,
+  },
+};
